86a6cc28fb40dd0807bdf8da9119800ac31ad6d2
Rohaan Nadeem <rohaan.nadeem2614@gmail.com>
2023-11-17 17:29:05 -0500

    simple operation test cases

--- part_2_calculator/src/test/java/org/example/CalculatorTest.java
+++ part_2_calculator/src/test/java/org/example/CalculatorTest.java
@@ -1,22 +1,31 @@
 package org.example;
+import junit.framework.TestCase;
 
-import org.junit.Before;
-import org.junit.Test;
+public class CalculatorTest extends TestCase {
 
-import static org.junit.Assert.*;
-
-public class CalculatorTest {
-
-    Calculator calculator;
-
-    @Before
-    public void setUp() {
-        calculator = new Calculator();
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        double[] sumArray = {1.5, 2.5, 3.5};
+        assertEquals(7.5, calculator.sum(sumArray), 0.001);
     }
 
-    @Test
-    public void divide() {
-        double result = calculator.divide(10, 2);
-        assertEquals(5, result, 0);
+    public void testSubtract() {
+        Calculator calculator = new Calculator();
+        double[] subtractArray = {5.0, 2.0, 1.0};
+        assertEquals(2.0, calculator.subtract(subtractArray), 0.001);}
+
+    public void testMultiplication() {
+        Calculator calculator = new Calculator();
+
+        // Test case for multiplication method
+        double[] multiplyArray = {2.0, 3.0, 4.0};
+        assertEquals(24.0, calculator.Multiplication(multiplyArray), 0.001); // Using delta for double comparison
     }
+
+    public void testDivide() {
+        Calculator calculator = new Calculator();
+        assertEquals(5.0, calculator.divide(10.0, 2.0), 0.001); }
+
+
+
 }
